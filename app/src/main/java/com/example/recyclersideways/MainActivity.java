41470e76b5fb09d06bf0e504de112a160f0a8da9
davidfitz314 <davidfitz314@gmail.com>
2019-03-19 00:56:16 -0600

    Initial commit

---
+++ app/src/main/java/com/example/recyclersideways/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.recyclersideways;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.LinearLayout;
+
+import java.util.LinkedList;
+
+public class MainActivity extends AppCompatActivity {
+    private final LinkedList<String> mWordList = new LinkedList<>();
+    private RecyclerView mRecyclerView;
+    private WordListAdapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        for (int i = 1;i<21; i++) {
+            mWordList.addLast("Word "+i);
+        }
+
+        //creating the recycler view
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        mRecyclerView = findViewById(R.id.recyclerview);
+        mAdapter = new WordListAdapter(this, mWordList);
+        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.setLayoutManager(layoutManager);
+    }
+}
